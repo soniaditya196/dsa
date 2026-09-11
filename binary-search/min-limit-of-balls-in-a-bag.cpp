254e222691b21e4d0e2d5a550636b41465c7d312
aditya <soniaditya196@gmail.com>
2026-09-11 07:27:26 +0530

    refactored and restructured code

---
+++ binary-search/min-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Input: nums = [2,4,8,2], maxOperations = 4
+Output: 2
+*/
+
+class Solution {
+    public:
+        int minimumSize(vector<int>& nums, int maxOperations) {
+            int n = nums.size(), ans;
+            int l = 1, r = *max_element(nums.begin(), nums.end());
+    
+            auto possible = [&] (int mid) {
+                long long int sum = 0;
+                for (int i = 0; i < n; i++) {
+                    sum += ( nums[i] - 1 ) / mid ;
+                }
+                return sum <= maxOperations;
+            };
+    
+            while (l <= r) {
+    
+                int mid = l + (r - l) / 2;
+    
+                if (possible(mid)) {
+                    r = mid - 1;
+                    ans = mid;
+                } else {
+                    l = mid + 1;
+                }
+    
+            }
+    
+            return ans;
+        }
+    };
+
+int main() {
+    return 0;
+}
